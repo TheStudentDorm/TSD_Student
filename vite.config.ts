c961fd19c371126ea3888ac49b1ab3ee4c2aa805
NIJI NAZIM <micky.sham@gmail.com>
2025-09-03 19:17:55 +0400

    Fix Vite config and update Navbar component

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: './',
+  resolve: {
+    alias: {
+      // Optional: Only needed if you have multiple React versions issues
+      // Use path.resolve to point to your node_modules react if required
+      react: path.resolve('./node_modules/react'),
+      'react-dom': path.resolve('./node_modules/react-dom'),
+    },
+  },
+});
